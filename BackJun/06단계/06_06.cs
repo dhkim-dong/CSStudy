7bf53c3799738a84788088fa53d08aea6c0eae73
unknown <dhkim20016@gmail.com>
2022-07-19 23:22:42 +0900

    2022/07/19 백준

---
+++ BackJun/06단계/06_06.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJun._06단계
+{
+    internal class _06_06
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] inputs = Console.ReadLine().Split();
+
+            sb.Append(inputs.Length.ToString());
+            Console.WriteLine(sb.ToString());
+
+            // 문장이 아니라 단어
+        }
+    }
+}
