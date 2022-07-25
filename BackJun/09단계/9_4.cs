33b61989f002a6d1740ea59eccadcc258315a08b
unknown <dhkim20016@gmail.com>
2022-07-25 22:39:43 +0900

    2022/07/25

--- BackJun/09단계/9_4.cs
+++ BackJun/09단계/9_4.cs
@@ -8,5 +8,43 @@ namespace BackJun._09단계
 {
     internal class _9_4
     {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if(isEmpty(i, j))
+                    {
+                        sb.Append(" ".ToString());
+                        continue;
+                    }
+                    else
+                    {
+                        sb.Append("*".ToString());
+                    }
+                }
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString());
+
+        }
+
+        static bool isEmpty(int x, int y)
+        {
+            while (x!=0)
+            {
+                if (x % 3 == 1 && y % 3 == 1)
+                    return true;
+
+                x /= 3;
+                y /= 3;
+            }
+            return false;
+        }
+       
     }
 }
