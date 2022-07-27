c3585961c12cf8e09875a2048c393e2deb04fc68
dhkim20016 <dhkim20016@gmail.com>
2022-07-27 17:13:31 +0900

    2022/07/27 백준

---
+++ BackJun/10단계/10_2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJun._10단계
+{
+    internal class _10_2
+    {
+        static void Main1(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string N = Console.ReadLine();
+            Console.WriteLine(generator(int.Parse(N))); 
+
+        }
+        
+        // 무한 반복으로 전체 값을 찾는 로직 => 시간초과 1부터~N까지 전부다 조사하기 때문에 시간이 n
+        //static int Publisher(int pub)
+        //{
+        //    for(int N = 1; true; N++)
+        //    {
+        //        if (pub == N + N % 10 + N / 10 % 10 + N / 100 % 10 + N / 1000 % 10 + N / 10000 % 10 + N / 100000 % 10 + N / 1000000 % 10)
+        //            return N;
+        //    }
+        //}
+
+        // 분해합을 구하는 함수
+        static int Publisher(string N)
+        {
+            List<int> value = new List<int>();
+
+            for (int i = 0; i < N.Length; i++)
+            {
+                value.Add(int.Parse(N[i].ToString()));
+            }
+            int result = 0;
+
+            foreach(var i in value)
+            {
+                result += i;
+            }
+
+            result += int.Parse(N);
+
+            return result;
+        }
+
+        // 생성자를 구하는 함수
+        static int generator(int num)
+        {
+            int temp = num - 1;             // 주어진 값보다 작은 값들을 temp에 저장한다. (정수값이기 때문에 1씩 감소)
+            int ans = num;                  // 현재값을 저장하는 변수
+            while(temp > 0)                 // temp값이 0이되면 종료한다.
+            {
+                if(Publisher(temp.ToString()) == num)           
+                {
+                    if (ans > temp) ans = temp;
+                }
+                temp--;
+            }
+            if (ans == num) return 0;       // 분해합 결과 값이 변함이 없으면 0을 반환한다.
+            else return ans;                // 결과가 있다면 그 ans값을 반환한다.
+        }
+
+        // 시간 복합도가 얼마나 빠른지 알고 싶다.
+    }
+}
---
+++ BackJun/10단계/10_3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJun._10단계
+{
+    internal class _10_3
+    {
+        static List<int> Hgroup = new List<int>();
+        static List<int> Wgroup = new List<int>();
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                Compare(N, i);
+            }
+        }
+
+        // 데이터 입력
+        static void InputData()
+        {
+
+        }
+
+
+        static int Compare(int n, int n1) // 비교 주체
+        {         
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputs = Console.ReadLine().Split();
+
+                int weight = int.Parse(inputs[0]);
+                int height = int.Parse(inputs[1]);
+
+                Hgroup.Add(height);
+                Wgroup.Add(weight);
+            }
+
+            int level = n;
+
+            for (int i = 0; i < n-1; i++)
+            {
+                if (n1 == i)
+                    continue;
+
+                if (Hgroup[n1] > Hgroup[i])
+                {
+                    level--;
+                }
+                else
+                    continue;
+            }
+
+            return level;
+        }
+    }
+}
