006371ab6d57ac72b8d9b095ec4d388c42790440
dhkim-dong <84676662+dhkim-dong@users.noreply.github.com>
2023-01-02 23:16:29 +0900

    backJun 15-2 commit

---
+++ BackJun/15단계/15_02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJun._15단계
+{
+    internal class _15_02
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] inputs = Console.ReadLine().Split();
+
+            int[] arr2 = Array.ConvertAll(inputs, s => int.Parse(s));
+
+            Array.Sort(arr2);
+
+            if (n == 1) // 약수가 하나밖에 없다면 (즉 약수가 2일 때 는 4!)
+            {
+                Console.WriteLine(arr2[0] * arr2[0]);
+            }
+            else // 나머지는 가장 큰 약수 x 가장 작은 약수 = 진 약수가 된다.
+            {
+                Console.WriteLine(arr2[n-1] * arr2[0]);
+            }
+
+        }
+    }
+}
